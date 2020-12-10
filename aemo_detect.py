78849b5df1de14916661916c6882cc972791fd18
anujapatil31 <66002540+anujapatil31@users.noreply.github.com>
2020-12-10 10:43:50 +0530

    Add files via upload

---
+++ aemo_detect.py
@@ -0,0 +1,27 @@
+import cv2
+
+
+bod_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+def detect(gray, frame):
+    body= bod_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in body:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
+        # roi_gray = gray[y:y+h, x:x+w]
+        # roi_color = frame[y:y+h, x:x+w]
+        #  = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        # for (ex, ey, ew, eh) in eyes:
+        #     cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+    return frame          
+    
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows() 
+
